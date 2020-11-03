d895732a1bd246bbc144b7cb8e9262c6b2f6a12f
huangzongbao <huangzongbao@huangzb-mac-pro.local>
2020-11-03 17:22:52 +0800

    多功能面板交互完成

--- src/services/goods.ts
+++ src/services/goods.ts
@@ -16,6 +16,15 @@ export function fetchCategoryList(): Promise<PostData> {
 export function saveGoods(data: GoodsInfo): Promise<PostData> {
   return axios.post(API.GOODS_SAVE, data)
 }
-export function fetchGoodsList(data: { current: number; size: number; status?: number; descOrders?: string[]; categoryId?: number }): Promise<PostData> {
+export function fetchGoodsList(data: { current: number; size: number; status?: number; descOrders?: string[]; categoryId?: number; name?: string }): Promise<PostData> {
   return axios.post(API.GOODS_LIST, data);
+}
+export function allGoodsOnDown(data: { isTrue: number }): Promise<PostData> {
+  return axios.post(API.GOODS_ON_DOWN, data)
+}
+export function batchGoodsOnDown(data: { isTrue: number; idList: any[] }): Promise<PostData> {
+  return axios.post(API.GOODS_BATCH_ON_DOWN, data);
+}
+export function delGood(data: { id: number }): Promise<PostData> {
+  return axios.post(API.GOOD_DEL, data);
 }
