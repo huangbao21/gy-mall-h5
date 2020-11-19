7aab4ce895a2eadfb5d2cdd85a203a9decf473d7
huangzongbao <huangzongbao@huangzb-mac-pro.local>
2020-11-19 14:15:44 +0800

    海报页

---
+++ src/views/poster/Introduce.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="introduce">
+    <img src="https://quanmingjx.oss-cn-shanghai.aliyuncs.com/images/f42b5d011be241018dc3b6f11241df5f" />
+  </div>
+</template>
+<style lang="scss" scoped>
+.introduce{
+  height: 100%;
+}
+img{
+  width: 100%;
+}
+</style>
