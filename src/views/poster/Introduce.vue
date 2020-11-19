2220929eb85347bdc8aea6c4c2b595b62b69c6c9
huangzongbao <huangzongbao@huangzb-mac-pro.local>
2020-11-19 14:44:43 +0800

    fix

--- src/views/poster/Introduce.vue
+++ src/views/poster/Introduce.vue
@@ -1,13 +1,41 @@
 <template>
-  <div class="introduce">
-    <img src="https://quanmingjx.oss-cn-shanghai.aliyuncs.com/images/f42b5d011be241018dc3b6f11241df5f" />
+  <div class="introduce nav-bar">
+    <van-nav-bar title="光鹰" :border="false" @click-left="toView">
+      <template #left>
+        <img class="leftIcon" src="@/assets/imgs/common/icon-left.png" />
+      </template>
+    </van-nav-bar>
+    <img
+      src="https://quanmingjx.oss-cn-shanghai.aliyuncs.com/images/f42b5d011be241018dc3b6f11241df5f"
+    />
   </div>
 </template>
+<script lang="ts">
+import useBackAppApi from "@/composables/useBackAppApi";
+
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Introduce",
+  setup() {
+    const { toBackApp } = useBackAppApi();
+    return {
+      toBackApp,
+    };
+  },
+  methods: {
+    toView() {
+      this.toBackApp();
+    },
+  },
+});
+</script>
 <style lang="scss" scoped>
-.introduce{
+@import "@/styles/base.scss";
+
+.introduce {
   height: 100%;
 }
-img{
+img {
   width: 100%;
 }
 </style>
